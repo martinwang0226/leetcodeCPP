34171357e2a550c5d688e725dba7b6a7a9fae20d
martin.wang <martin.wang@cootek.com>
2020-02-15 12:11:16 +0800

    shuati

---
+++ leetcodeCPP/shuati/0001-Two-Sum/cpp-0001/cmake-build-debug/CMakeFiles/cpp_0001.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_0001.dir/main.cpp.o"
+  "cpp_0001"
+  "cpp_0001.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_0001.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
